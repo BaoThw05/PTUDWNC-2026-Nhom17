097d928468279b070910db080921ff6d36c23cfb
2312776 <2312776@dlu.edu.vn>
2026-09-22 18:39:20 +0700

    feat: Recipe domain entities + IAppDbContext (khung moi tu main) - kien truc test xanh

---
+++ src/Backend/CulinaryBlog.Application/Abstractions/IAppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CulinaryBlog.Domain.Common;
+using CulinaryBlog.Domain.Entities;
+
+namespace CulinaryBlog.Application.Abstractions;
+
+public interface IAppDbContext
+{
+    IQueryable<Recipe> Recipes { get; }
+    IQueryable<RecipeStep> RecipeSteps { get; }
+    IQueryable<RecipeIngredient> RecipeIngredients { get; }
+
+    void Add<TEntity>(TEntity entity) where TEntity : class;
+    void Remove<TEntity>(TEntity entity) where TEntity : class;
+
+    void SetOriginalVersion<TEntity>(TEntity entity, uint version) where TEntity : BaseEntity;
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
